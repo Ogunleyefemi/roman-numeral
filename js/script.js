61b698ee8ecb3265d4bcb31e26f92ec6d9727524
femi <oluwafemiogunleye21@gmail.com>
2022-09-20 08:41:29 -0700

    change values

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 function convert(num) { 
-  if (num ==! number){
-    return NaN
-  };
     if(num < 1){ return "";}
-    if(num >= 40){ return "XL" + convert(num - 40);}
+    if(num >= 1000){ return "M" + convert(num - 1000);}
+    if(num >= 500){ return "D" + convert(num - 500);}
+    if(num >= 100){ return "C" + convert(num - 100);}
+    if(num >= 50){ return "L" + convert(num - 50);}
     if(num >= 10){ return "X" + convert(num - 10);}
-    if(num >= 9){ return "IX" + convert(num - 9);}
     if(num >= 5){ return "V" + convert(num - 5);}
-    if(num >= 4){ return "IV" + convert(num - 4);}
-    if(num >= 1){ return "I" + convert(num - 1);}  
-  }
+    if(num >= 1){ return "I" + convert(num - 1);} 
+    return convert(num); 
+  };
+  
   // console.log(convert(num));
 
   $(document).ready(function() {
     $("#form").submit(function(event) {
       event.preventDefault();
       const number = $("#input").val();
-      const result = convert(num);
-      $("#result").html(result);
-    })
+      $("#result").html(convert(num));
+    });
   });
